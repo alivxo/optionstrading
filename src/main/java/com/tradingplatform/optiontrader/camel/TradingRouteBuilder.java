3672cd944b9c7c22c05ea232c33e0f4b69ecd52b
Alivxo <avsal0218@gmail.com>
2025-04-04 17:19:26 -0400

    class variables and html files

--- src/main/java/com/tradingplatform/optiontrader/camel/TradingRouteBuilder.java
+++ src/main/java/com/tradingplatform/optiontrader/camel/TradingRouteBuilder.java
@@ -1,5 +1,18 @@
 package com.tradingplatform.optiontrader.camel;
 
-public class TradingRouteBuilder {
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TradingRouteBuilder extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer:fetchOptions?period=60000")
+                .to("bean:apiService?method=fetchOptions")
+                .log("Fetched options: ${body}");
+
+        from("timer:fetchTrades?period=60000")
+                .to("bean:apiService?method=fetchTrades")
+                .log("Fetched trades: ${body}");
+    }
 }
-// this class is responsible for defining the routes for the Camel context
